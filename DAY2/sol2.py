e6e2e643604980701a4eeefc66b27b14dfbdbfaf
Christopher <christopher.akersten@gmail.com>
2017-12-02 16:45:46 +0100

    oh, there are two parts? Added part 2 solution 2

--- DAY2/sol2.py
+++ DAY2/sol2.py
@@ -1,4 +1,4 @@
-def checksum(input : list):
+def checksumP1(input : list):
     checksum = 0
     for row in input:
         max = min = row[0]
@@ -8,4 +8,18 @@ def checksum(input : list):
             if(c < min):
                 min = c
         checksum += max - min
+    return checksum
+
+def checksumP2(input : list):
+    checksum = 0
+    for row in input:
+        for c in row:
+            rowCheckSum = None
+            for cc in row:
+                if not c % cc and not c is cc:
+                    rowCheckSum = c / cc
+                    break
+            if rowCheckSum is not None:
+                checksum += rowCheckSum
+                break
     return checksum
--- DAY2/test.py
+++ DAY2/test.py
@@ -1,13 +1,22 @@
 import unittest
-from DAY2.sol2 import checksum
+
+from DAY2.sol2 import *
 
 
 class TestSolution2(unittest.TestCase):
-    def test_sol2(self):
+    def test_sol2part1(self):
         input = "5 1 9 5 \n 7 5 3 \n 2 4 6 8"
         input = [[int(i) for i in row.split()] for row in input.splitlines() ]
         result = 18
-        self.assertEquals(checksum(input), result)
+
+        self.assertEquals(checksumP1(input), result)
+
+    def test_sol2part2(self):
+        input = "5 9 2 8 \n 9 4 7 3 \n 3 8 6 5"
+        input = [[int(i) for i in row.split()] for row in input.splitlines()]
+        result = 9
+
+        self.assertEquals(checksumP2(input), result)
 
 if __name__ == '__main__':
     unittest.main()
